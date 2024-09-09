803762b84dd743b45d941d949a2015e3b4032419
Adesh Malunjkar <adesh.malunjkar@smartdocs.ai>
2024-09-09 10:27:51 +0530

    initial commit

---
+++ lamda/src/main/java/com/example/PlayWith_Comparator.java
@@ -0,0 +1,41 @@
+package com.example;
+
+import com.example.model.Person;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Function;
+
+public class PlayWith_Comparator {
+
+
+    public static void main(String[] args) {
+        Person ravindra = new Person("Ravindra", "baloda", 37);
+        Person deepak = new Person("Deepak", "Dev", 27);
+        Person aakash = new Person("Aakash", "Sharma", 24);
+        Person pooja = new Person("Pooja", "Sharma", 32);
+        Person sunil = new Person("Sunil", "Kichar", 36);
+        Person ashish=new Person("Ashish","Chaudhary",33);
+
+        Function<Person,String> firstName = Person::getFirstName;
+        Function<Person,String> lastName = Person::getLastName;
+        Function<Person,Integer> age = Person::getAge;
+
+        Comparator<Person> comparator = Comparator.comparing(firstName)
+                .thenComparing(lastName)
+                .thenComparing(age);
+
+        comparator = Comparator.nullsLast(comparator);
+
+
+        List<Person> peoples = Arrays.asList(ravindra, null, aakash,deepak,pooja,sunil,ashish);
+        peoples.sort(comparator);
+
+
+        peoples.forEach(System.out::println);
+
+
+    }
+}
---
+++ lamda/src/main/java/com/example/model/Person.java
@@ -0,0 +1,17 @@
+package com.example.model;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+//@NoArgsConstructor
+@ToString
+public class Person {
+
+
+    private String firstName;
+    private String lastName;
+    private int age;
+}
